8600cc42c39659a3e68e684935d3f845fe5900b3
Aryan Raj Singh <aryanrs693@gmail.com>
2025-01-06 12:37:00 +0530

    Adding snippet for difference

--- p1.py
+++ p1.py
@@ -6,3 +6,5 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 result = num1 + num2
 print(f"The sum of {num1} and {num2} is {result}.")
+result2 = num1 - num2
+print(f"The difference of {num1} and {num2} is {result2}.")
